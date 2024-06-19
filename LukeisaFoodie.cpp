3398cfc82950a1f9287f776734488b08efdc46c4
Nabeel Ahsan <104347823+DividedByyZero@users.noreply.github.com>
2024-06-20 01:23:09 +0600

    Create LukeisaFoodie.cpp
    brute-force,greedy,implementation

---
+++ LukeisaFoodie.cpp
@@ -0,0 +1,34 @@
+// problem link : https://codeforces.com/problemset/problem/1704/B
+// tag : brute-force,greedy,implementation
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t; cin >> t;
+	while(t--){
+		int n,x; cin >> n >> x;
+		vector<int>v(n);
+		for(auto &x : v) cin >> x;
+		
+		int ans = 0;
+		int high = v[0]+x;
+		int low = v[0]-x;
+		for(auto element : v){
+			int h = element+x;
+			int l = element-x;
+			if(!(h<=high && h>=low ) && !(l<=high && l>=low) && !(l<=low && h>=high)){
+				// cout << low << " " << high << endl;
+				high = h;
+				low = l;
+				ans++;
+				// cout << low << " " << high << endl;
+			}
+			else{
+				high = min(h,high);
+				low = max(l,low);
+			}
+		}
+		cout << ans << endl;
+
+	} 
+}
